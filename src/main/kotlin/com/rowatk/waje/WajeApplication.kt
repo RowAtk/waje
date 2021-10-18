61b4a75a048d21fae0526d8b05bec693dce4754c
Rowan Atkinson <rowaneatkinson@gmail.com>
2021-10-18 14:48:19 -0500

    initial project

---
+++ src/main/kotlin/com/rowatk/waje/WajeApplication.kt
@@ -0,0 +1,11 @@
+package com.rowatk.waje
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WajeApplication
+
+fun main(args: Array<String>) {
+	runApplication<WajeApplication>(*args)
+}
---
+++ src/test/kotlin/com/rowatk/waje/WajeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rowatk.waje
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WajeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
